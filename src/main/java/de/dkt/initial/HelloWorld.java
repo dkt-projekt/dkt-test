e290b5c90a6456cced0cf061788a42984033286b
jmschnei <jmschnei@jmschnei-laptop>
2015-11-19 14:57:06 +0100

    Initial class of the hello world example

---
+++ src/main/java/de/dkt/initial/HelloWorld.java
@@ -0,0 +1,16 @@
+package de.dkt.initial;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+	}
+	
+	public String getMessageFor(String s){
+		return "Hello World for "+s+"!!!";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world test for Jenkis!!!");
+	}
+
+}
